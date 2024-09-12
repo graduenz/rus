7dfbe019ee41242ead07ab3cfc4abc786a5fb434
Guilherme Raduenz <graduenz@gmail.com>
2024-09-12 10:11:24 -0300

    chore: implemented advertiser dto validator

--- backend/Advertiser.Application/AdvertiserDtoValidator.cs
+++ backend/Advertiser.Application/AdvertiserDtoValidator.cs
@@ -1,8 +1,13 @@
 ﻿using FluentValidation;
+using FluentValidation.Validators;
 
 namespace Advertiser.Application;
 
 public class AdvertiserDtoValidator : AbstractValidator<AdvertiserDto>
 {
-    // TODO: Setup validation
+    public AdvertiserDtoValidator()
+    {
+        RuleFor(m => m.Name).NotEmpty().MinimumLength(3);
+        RuleFor(m => m.Email).EmailAddress().When(m => m.Email is not null);
+    }
 }
