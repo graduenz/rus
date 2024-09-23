fb516cc920c35353db3a1641ea85b24be049b234
Guilherme Raduenz <graduenz@gmail.com>
2024-09-22 22:37:14 -0300

    chore: changed BaseEntity Id default to use UUID v7

--- backend/src/Rus.Base.Domain/BaseEntity.cs
+++ backend/src/Rus.Base.Domain/BaseEntity.cs
@@ -2,7 +2,7 @@
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.CreateVersion7();
     public DateTime CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
     public DateTime ModifiedAt { get; set; }
